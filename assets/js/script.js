47d9878c2f497ccd5a2c84b1e3175405a3ee47a1
Omair Muhi <omairmuhi@icloud.com>
2020-11-09 07:53:35 -0500

    Subject: Complete init() routine using stubs Problem: NA Solution: NA Note: NA

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,7 @@ var detailedWeather = {
     uvIndex: 0,
     summaryWeather: {},
 }
+var searchHistoryList = ["Toronto"];
 
 // HANDLER FUNCTIONS
 function searchButtonHandler(event) {
@@ -21,6 +22,7 @@ function searchButtonHandler(event) {
 function searchHistoryButtonHandler(event) {
 
 }
+
 // LOCAL-DB FUNCTIONS
 /**
  * Function: saveCityDataToLocalDb
@@ -38,6 +40,7 @@ function loadCityDataFromLocalDb() {}
  *  needs to be refreshed.
  */
 function isLocalDbStale() {}
+
 // RENDER FUNCTIONS
 /**
  * Function: renderHistoryList
@@ -54,11 +57,60 @@ function renderCurrentWeather() {}
  * Description: Populate BS cards with 5-day outlook
  */
 function renderFutureWeather() {}
+
+// API FUNCTIONS
+/**
+ * Function: extractCurrentWeatherData
+ * Description: Extract weather data from JSON data
+ * Parameters: jsonData - data returned by API call
+ */
+function extractCurrentWeatherData(jsonData) {}
+/**
+ * Function: retrieveCurrentWeather
+ * Description: Make an API call to get today's weather
+ * Parameters: cityString - name of city
+ * Returns: JSON weather data
+ */
+function retrieveCurrentWeather(cityString) {}
+/**
+ * Function: extractFutureWeatherData
+ * Description: Extract future weather data from JSON data
+ * Parameters: jsonData - data returned by API call
+ */
+function extractFutureWeatherData(jsonData) {}
+/**
+ * Function: retrieveFutureWeather
+ * Description: Make an API call to get 5-day outlook
+ * Parameters: cityString - name of city
+ * Returns: JSON weather data
+ */
+function retrieveFutureWeather(cityString) {
+
+}
+
 // INIT FUNCTIONS
 /**
  * Function: initApplication
  * Description: Entry-point for the weather-dashboard application
  */
-function initApplication() {}
+function initApplication() {
+    // display Toronto weather on page refresh
+    var defaultCity = "Toronto";
+    // get today's weather
+    var currentWeatherJson = retrieveCurrentWeather(defaultCity);
+    extractCurrentWeatherData(currentWeatherJson);
+    // render today's weather
+    renderCurrentWeather();
+    // get 5-day outlook
+    var futureWeatherJson = retrieveFutureWeather(defaultCity);
+    extractFutureWeatherData(futureWeatherJson);
+    // render 5-day outlook
+    renderFutureWeather();
+    // add city to search history
+    if (searchHistoryList.includes(defaultCity) === false) {
+        searchHistoryList.push(defaultCity);
+        renderHistoryList();
+    }
+}
 // jQuery entry-point
 $(document).ready(initApplication);
